275538ce80f4ae6df66e66a7c09967ec4911c357
mramavaishnavi <140266799+mramavaishnavi@users.noreply.github.com>
2024-03-04 09:47:33 +0530

    Create angular.service.ts

---
+++ 20981a05a0FSAssignments/fsassignment2/angular.service.ts
@@ -0,0 +1,6 @@
+export class AngularService{
+    getCourses(){
+        return ["course1: ARTIFICAL INTELLIGENCE--Analyzing of Artifical Intelligence","course2: BASIC C--Learning of C programming","course3: ACCOUNTING--classifying the financial assets",
+        "course4: JAVA--developing applications"];
+    }
+}
